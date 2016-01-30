0893fa63f6e62a5c041b46a68f6baa2a23f0627c
Milos Stanojevic <ms2239@cam.ac.uk>
2016-01-30 13:18:47 +0000

    location wrapper class added

--- client/src/com/hackbridge/viral/SimpleUpActivity.java
+++ client/src/com/hackbridge/viral/SimpleUpActivity.java
@@ -14,26 +14,17 @@
  * limitations under the License.
  */
 
-package com.example.android.appnavigation.app;
+package com.hackbridge.viral;
 
 import com.example.android.appnavigation.R;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class ViewFromOtherTaskActivity extends Activity {
+public class SimpleUpActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_from_other_task);
-    }
-
-    public void onLaunchOtherTask(View v) {
-        Intent target = new Intent(this, OutsideTaskActivity.class)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
-                        Intent.FLAG_ACTIVITY_TASK_ON_HOME);
-        startActivity(target);
+        setContentView(R.layout.simple_up);
     }
 }
