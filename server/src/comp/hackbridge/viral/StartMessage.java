06748e7ea3a522f3bcfaa20ec614fe4da2983a6e
Petar Veličković <pv273@cam.ac.uk>
2016-01-30 11:45:01 +0000

    States implemented

---
+++ server/src/comp/hackbridge/viral/StartMessage.java
@@ -0,0 +1,13 @@
+package comp.hackbridge.viral;
+
+/**
+ * Created by PetarV on 30/01/2016.
+ */
+
+/*
+    Server -> Phone
+    (new_physical_state, new_ new_id)
+ */
+public class StartMessage extends Message {
+
+}
