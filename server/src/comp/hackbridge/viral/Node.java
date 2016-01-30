a7513bb8d0afa9235ce4f53d036269d64d3b123f
Stella Lau <sl715@cam.ac.uk>
2016-01-30 13:18:24 +0000

    Update Node properties.

--- server/src/comp/hackbridge/viral/Node.java
+++ server/src/comp/hackbridge/viral/Node.java
@@ -2,13 +2,27 @@ package comp.hackbridge.viral;
 
 public class Node {
     private static long GLOBAL_ID = 1;
-    private long nodeID;
+    private final long nodeID;
 
-    public Node() {
+    private PhysicalState physical_state;
+    private AwarenessState awareness_state;
 
+    public Node(PhysicalState physical_s, AwarenessState awareness_s) {
+        nodeID = GLOBAL_ID;
+        GLOBAL_ID++;
+        this.physical_state = physical_s;
+        this.awareness_state = awareness_s;
     }
 
     public long getID() {
         return nodeID;
+    }
+
+    public AwarenessState getAwarenessState() {
+        return awareness_state;
+    }
+
+    public PhysicalState getPhysicalState() {
+        return physical_state;
     }
 }
