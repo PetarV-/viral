d593886896847ccdfef875adbbd316ec9837362a
Petar Veličković <pv273@cam.ac.uk>
2016-01-30 16:37:09 +0000

    Work on the client handler.

--- server/src/com/hackbridge/viral/ClientHandler.java
+++ server/src/com/hackbridge/viral/ClientHandler.java
@@ -22,8 +22,35 @@ public class ClientHandler {
         this.queue = queue;
     }
 
-    public ClientHandler fromSocket(Socket socket) {
+    public static IdHandlerPair fromSocket(Socket socket, LocationState locState, ConcurrentLinkedQueue<Message> queue) {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            Message m = (Message)ois.readObject();
+            StartMessage smsg;
+            long id;
+            if (m instanceof HelloNewMessage) {
+                smsg = locState.OnConnect();
+                id = smsg.getId();
+            } else if (m instanceof HelloMessage) {
+                HelloMessage hm = (HelloMessage)m;
+                smsg = locState.OnConnect(hm.getId());
+                id = hm.getId();
+            } else {
+                ois.close();
+                oos.close();
+                return null;
+            }
+            oos.writeObject(smsg);
+            return new IdHandlerPair(id, new ClientHandler(ois, oos, id, queue));
 
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public void listen() {
--- server/src/com/hackbridge/viral/Main.java
+++ server/src/com/hackbridge/viral/Main.java
@@ -1,14 +1,19 @@
 package com.hackbridge.viral;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
+    private static LocationState locState;
+    private static ConcurrentLinkedQueue<Message> queue;
+    private static Map<Long, ClientHandler> handlers = new HashMap<>();
+
     // Starts a round
     public static void startRound() {
 
@@ -25,37 +30,28 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        try {
-            System.out.println(InetAddress.getLocalHost());
-            ServerSocket ss = new ServerSocket(25000);
-            Socket s = ss.accept();
-            InputStream is = s.getInputStream();
-            ObjectInputStream ois = new ObjectInputStream(is);
-            Message m = (Message)ois.readObject();
-            if (m instanceof HelloNewMessage) {
-                System.out.println("YAY");
+        locState = new LocationState();
+        queue = new ConcurrentLinkedQueue<>();
+        Thread input = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(InetAddress.getLocalHost());
+                    ServerSocket ss = new ServerSocket(25000);
+                    while (true) {
+                        Socket s = ss.accept();
+                        IdHandlerPair ihp = ClientHandler.fromSocket(s, locState, queue);
+                        if (ihp != null) {
+                            handlers.put(ihp.getId(), ihp.getHandler());
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            StartMessage msg = new StartMessage(9876543, PhysicalState.SUSCEPTIBLE, AwarenessState.UNAWARE);
-            System.out.println("Accepted socket!");
-            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
-            oos.writeObject(msg);
-            System.out.println("KEK");
-            m = (Message)ois.readObject();
-            System.out.println("WEW");
-            if (m instanceof HelloMessage) {
-                HelloMessage hm = (HelloMessage)m;
-                System.out.println(hm.getId());
-            }
-            //ois.close();
-            //oos.close();
-            System.out.println("SENT");
-            while (true) { }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        //new LocationStateTest();
-	// write your code here
+        };
+        input.setDaemon(true);
+        input.start();
+
     }
 }
