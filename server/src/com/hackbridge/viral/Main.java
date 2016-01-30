ee16e4b7908a14255b6e5a5e14a57d1682434b41
Petar Veličković <pv273@cam.ac.uk>
2016-01-30 14:24:39 +0000

    Made the methods static

--- server/src/com/hackbridge/viral/Main.java
+++ server/src/com/hackbridge/viral/Main.java
@@ -1,24 +1,43 @@
 package com.hackbridge.viral;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     // Starts a round
-    public void startRound() {
+    public static void startRound() {
 
     }
 
     // Stop a round
-    public void stopRound() {
+    public static void stopRound() {
 
     }
 
     // Send a message (ChangeMessage)
-    public void changeState(ChangeMessage chg) {
+    public static void changeState(ChangeMessage chg) {
 
     }
 
+
+
     public static void main(String[] args) {
-        new LocationStateTest();
+        try {
+            ServerSocket ss = new ServerSocket(1500);
+            Socket s = ss.accept();
+            StartMessage msg = new StartMessage(1, PhysicalState.SUSCEPTIBLE, AwarenessState.UNAWARE);
+            System.out.println("Accepted socket!");
+            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+            oos.writeObject(msg);
+            oos.close();
+            System.out.println("SENT");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //new LocationStateTest();
 	// write your code here
     }
 }
