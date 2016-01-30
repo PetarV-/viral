5c45bdb2da0b7f8ca81d2502259ea32ffbb32857
Stella Lau <sl715@cam.ac.uk>
2016-01-30 12:18:11 +0000

    Add LocationState method stubs.

--- server/src/comp/hackbridge/viral/LocationState.java
+++ server/src/comp/hackbridge/viral/LocationState.java
@@ -1,36 +1,61 @@
 package comp.hackbridge.viral;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class LocationState {
     private final int MAX_NODES = 1000; // TODO
     private final int array_capacity = 100;
-    private final ArrayList<ArrayList<Double>> state;
+    private final ArrayList<ArrayList<Double>> state; //2D state array
+    private HashMap<Long, Node> nodes;  // Map from node ID to Node
+    private HashMap<Long, Integer> state_position;  // Map from node ID to array pos
 
     private int num_nodes = 0;
 
     public LocationState() {
         state = new ArrayList<ArrayList<Double>>();
+        nodes = new HashMap<Long, Node>();
+        state_position = new HashMap<Long, Integer>();
         for (int i = 0; i < array_capacity; ++i) {
             state.add(new ArrayList<Double>());
         }
     }
 
-    public void OnConnect() {
-        // TODO: add to hashmap of player to num
-        // TODO: if new player
+    /**
+     * Called upon addition of a new node.
+     */
+    public StartMessage OnConnect() {
+        return null;
+    }
+
+    /**
+     * Called upon connection of an existing node.
+     * @param id
+     * @return
+     */
+    public StartMessage OnConnect(long id) {
+
         if (num_nodes > array_capacity) {
 
         }
         System.out.println("Connected");
-
+        return null;
     }
 
-    public void OnDisconnect() {
+    /**
+     * Called upon disconnection of an existing node.
+     */
+    public boolean OnDisconnect(long id) {
         System.out.println("Disonnected");
+        return false;
     }
 
-    // TODO
-    public void OnLocationChange() {
+    /**
+     * @param nodeID
+     */
+    public void OnLocationChange(long nodeID) {
         System.out.println("Location Changed");
     }
+
 }
