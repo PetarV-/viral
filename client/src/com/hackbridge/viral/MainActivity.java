fc11ad87b38d61bee942894b846734e8c045c39f
Andrej Ivašković <dreian95@gmail.com>
2016-01-31 06:17:07 +0000

    First notification system

--- client/src/com/hackbridge/viral/MainActivity.java
+++ client/src/com/hackbridge/viral/MainActivity.java
@@ -52,6 +52,33 @@ public class MainActivity extends Activity
         round_on = isOn;
     }
 
+    public void writeNotification(String title, String body)
+    {
+        NotificationCompat.Builder mBuilder =
+            new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.syringe_left)
+            .setContentTitle(title)
+            .setContentText(body);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        //
+        // // The stack builder object will contain an artificial back stack for the
+        // // started Activity.
+        // // This ensures that navigating backward from the Activity leads out of
+        // // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(ResultActivity.class);
+        // // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+        stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(mId, mBuilder.build());
+    }
+
     public PhysicalState loadPhysicalState()
     {
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
@@ -100,9 +127,8 @@ public class MainActivity extends Activity
                 editor.putInt("physical", 0);
                 break;
             case VACCINATED:
-                if (oldState != PhysicalState.VACCINATED) Toast.makeText(
-                        MainActivity.this, "Successful vaccination!",
-                        Toast.LENGTH_SHORT).show();
+                if (oldState != PhysicalState.VACCINATED)
+                    writeNotification("Success!", "Your vaccination has been successful!");
                 editor.putInt("physical", 1);
                 break;
             case INFECTED:
