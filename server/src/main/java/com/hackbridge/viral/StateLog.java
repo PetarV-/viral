af8bfe2251a98596f2644a54d980f47f94cbeeab
Petar Veličković <pv273@cam.ac.uk>
2016-03-25 12:29:11 +0000

    Update to logging subroutine.
    # Conflicts:
    #	server/.idea/workspace.xml

--- server/src/main/java/com/hackbridge/viral/StateLog.java
+++ server/src/main/java/com/hackbridge/viral/StateLog.java
@@ -13,11 +13,14 @@ import java.util.Map;
  */
 public class StateLog {
     private Map<Long, Node> nodes;
+    private Map<Long, Integer> positionMap;
     private List<ArrayList<Double>> state;
 
-    public StateLog(Map<Long, Node> nodes, List<ArrayList<Double>> state) {
+    public StateLog(Map<Long, Node> nodes, Map<Long, Integer> positionMap, List<ArrayList<Double>> state) {
         this.nodes = new HashMap<Long, Node>();
         this.nodes.putAll(nodes);
+        this.positionMap = new HashMap<Long, Integer>();
+        this.positionMap.putAll(positionMap);
         this.state = new ArrayList<ArrayList<Double>>();
         int i = 0;
         for (ArrayList<Double> row : state) {
@@ -43,7 +46,8 @@ public class StateLog {
             logfile.write(String.format("%d %d\n", nodes.size(), nodes.size()));
             for (Node node : nodes.values()) {
                 logfile.write(String.format(
-                        "%d %s %s\n", node.getID(), node.getAwarenessState().toString().charAt(0),
+                        "%d %d %s %s\n", node.getID(), positionMap.get(node.getID()),
+                        node.getAwarenessState().toString().charAt(0),
                         node.getPhysicalState().toString().charAt(0)));
             }
             for (ArrayList<Double> row : state) {
