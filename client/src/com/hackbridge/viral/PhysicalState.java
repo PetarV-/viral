a94c525ffe8be32dbc868b1854b7fba18224489d
Petar Veličković <pv273@cam.ac.uk>
2016-03-26 23:02:44 +0000

    Updated PhysicalState across clients.

--- client/src/com/hackbridge/viral/PhysicalState.java
+++ client/src/com/hackbridge/viral/PhysicalState.java
@@ -4,5 +4,8 @@ package com.hackbridge.viral;
  * Created by PetarV on 30/01/2016.
  */
 public enum PhysicalState {
-    SUSCEPTIBLE, VACCINATED, INFECTED
+    SUSCEPTIBLE,
+    VACCINATED,
+    INFECTED,
+    CARRIER  // Asymptomatic carrier - the node carries the disease agent but does not know it.
 }
